c9551bea27affd475487da002532f88963805f93
brandondando <dandobrandon0@gmail.com>
2024-04-26 13:21:03 +0300

    commit

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,25 @@
 import React from "react";
 
-function About() {
+function About(bio) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {bio.bio}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+      <h3>Links</h3>
+      <a href='https://github.com/liza'>{bio.github} </a>
+      <a href='https://www.linkedin.com/in/liza/'>{bio.linkedin} </a>
+      
+      
     </div>
   );
 }
 
 export default About;
+
+
+
+
+
+
